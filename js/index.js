65a427f56b945448c5c8c230b1e867d1eccacb83
Tomáš Taro <tomas@getadblock.com>
2018-10-11 20:21:41 +0200

    Initial import of the Meet and Code 2018 website

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const ONE_HOUR_IN_PIXELS = 200;
+const ONE_MINUTE_IN_PIXELS = ONE_HOUR_IN_PIXELS / 60;
+const TOP_NAME_IN_PIXELS = 100;
+
+function setTimemarker() {
+    const now = new Date();
+
+    const hours = now.getHours() - 10;
+
+    if (hours < 0 || hours > 7) {
+        $("#time").hide();
+        return;
+    }
+
+    const minutes = now.getMinutes();
+
+    const topPosition = hours * ONE_HOUR_IN_PIXELS + minutes * ONE_MINUTE_IN_PIXELS + TOP_NAME_IN_PIXELS;
+
+    $("#time").css("top", topPosition);
+}
+
+setTimemarker();
+
+setInterval(() => {
+    setTimemarker();
+}, 60 * 1000);
